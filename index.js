f6bd8c843f0bd509c290b42beb66b814a269e68e
Mark Frederiksen <mfrederiksen@gmail.com>
2017-07-09 21:37:52 -0400

    Got input validation tests passing

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const typeMarkupMap = {
 };
 
 function estimate(basePrice, numPeople, type) {
+  if (basePrice < 0) throw new Error('basePrice must be greater than 0');
+  if (numPeople < 0) throw new Error('numPeople must be greater than 0');
+
   let baseWithFlat = basePrice * flatMarkup;
-  let typeMarkup = typeMarkupMap[type] || 0;
+  let typeMarkup = typeMarkupMap[type.toLowerCase()] || 0;
   let finalEstimate = (baseWithFlat * (1.0 + (numPeople * peopleMarkup))) + (baseWithFlat * typeMarkup);
 
   // Round to 2 decimal points
