031c98670e63b6d6640577233682edb1c5c2b01f
Mark Frederiksen <mfrederiksen@gmail.com>
2017-07-09 21:36:12 -0400

    Added more tests

--- test/index.js
+++ test/index.js
@@ -18,7 +18,28 @@ describe('estimate', () => {
     estimate(0, 1, 'food').should.equal(0);
   });
 
-  it('Should return base price with base markup when people is 0 and type is other', () => {
+  it('Should return base price with base markup when people is 0 and type is unknown', () => {
     estimate(111.11, 0, '???').should.equal(116.67);
   });
+
+  it('Should not apply type markup when type is unknown', () => {
+    estimate(111.11, 1, '???').should.equal(118.07);
+  });
+
+  it('Should not apply people markup when people is 0', () => {
+    estimate(111.11, 0, 'food').should.equal(131.83);
+  });
+
+  it('Should allow case insensitive types', () => {
+    estimate(5432.00, 1, 'DruGs').should.equal(6199.81);
+  });
+
+  it('Should not allow negative base price', () => {
+    (() => estimate(-1, 1, 'food')).should.throw(Error);
+  });
+
+  it('Should not allow negative people', () => {
+    (() => estimate(0, -1, 'food')).should.throw(Error);
+  });
+
 });
